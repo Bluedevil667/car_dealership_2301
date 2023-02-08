4b58068f588daa46868579d0d32df74d22a6a23e
Logan Wilson <loganwilson777@gmail.com>
2023-02-08 09:37:18 -0700

    car class with make method

---
+++ lib/car.rb
@@ -0,0 +1,11 @@
+class Car
+  def initialize(make_model, monthly_payment, loan_length)
+    @make_model = make_model
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+  end
+
+  def make
+    @make_model.split.first
+  end
+end
